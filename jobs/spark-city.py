fa13766a1769685b8f1b7f3bd5fcd160e6ad979e
Dr. Jody-Ann S. Jones <44002819+drjodyannjones@users.noreply.github.com>
2024-03-05 16:58:26 +0100

    Update spark-city.py

--- jobs/spark-city.py
+++ jobs/spark-city.py
@@ -10,7 +10,7 @@ def main():
         .config("spark.jars.packages",
                 "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1," +  
                 "org.apache.hadoop:hadoop-aws:3.3.1," +
-                "com.amazonaws:aws-java-sdk-bundle:1.11.375") \
+                "com.amazonaws:aws-java-sdk:1.11.469") \
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
         .config("spark.hadoop.fs.s3a.access.key", configuration.get("AWS_ACCESS_KEY")) \
         .config("spark.hadoop.fs.s3a.secret.key", configuration.get("AWS_SECRET_KEY")) \
